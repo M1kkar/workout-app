962b8b8cf69328177bff1a7de58217c611b054cf
M1kkar <mikkarol39@gmail.com>
2021-11-09 13:51:58 +0100

    Add new files

---
+++ src/main/java/workout/workoutapp/database/entities/Users.java
@@ -0,0 +1,15 @@
+package workout.workoutapp.database.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Users {
+
+}
---
+++ src/test/java/workout/workoutapp/WorkoutBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package workout.workoutapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

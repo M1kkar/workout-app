f54e26084b1a8a1b8a30b1ada52cfee5df961cd4
M1kkar <mikkarol39@gmail.com>
2021-11-09 13:58:54 +0100

    test1

--- src/main/java/workout/workoutapp/database/entities/Users.java
+++ src/main/java/workout/workoutapp/database/entities/Users.java
@@ -4,12 +4,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
 @Getter
 @Setter
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id", nullable = false)
+    private Long user_id;
 
 }
